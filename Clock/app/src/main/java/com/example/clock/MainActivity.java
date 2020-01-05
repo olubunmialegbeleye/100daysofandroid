817a5db7a704db1eb432be61acb080f0bce74c4e
Olubunmi Alegbeleye <alegbeleyebunmi@gmail.com>
2020-01-05 22:03:28 +0100

    Got system time and formatted with SimpleDateFormatter. Also changed ViewGroup to RelativeLayout

--- Clock/app/src/main/java/com/example/clock/MainActivity.java
+++ Clock/app/src/main/java/com/example/clock/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.clock;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.widget.TextView;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import android.os.Bundle;
 
@@ -10,5 +13,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        while(true){
+            TextView timeTextView = (TextView) findViewById(R.id.timeTextView);
+            TextView dateTextView = (TextView) findViewById(R.id.dateTextView);
+            Date time = new Date();
+            SimpleDateFormat timeFormatter = new SimpleDateFormat("hh:mm:ss");
+            SimpleDateFormat dateFormatter = new SimpleDateFormat("EEEE MMMM dd, yyyy");
+            timeTextView.setText(timeFormatter.format(time));
+            dateTextView.setText(dateFormatter.format(time));
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
