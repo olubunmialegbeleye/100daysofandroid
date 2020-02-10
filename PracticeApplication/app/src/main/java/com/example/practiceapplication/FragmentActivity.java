cc51dd8711b9ee2d77284b2fde14c950b9c98061
Olubunmi Alegbeleye <alegbeleyebunmi@gmail.com>
2020-02-10 14:42:50 +0100

    Playing around with inflating fragment from xml ui file using the <fragment> tag

---
+++ PracticeApplication/app/src/main/java/com/example/practiceapplication/FragmentActivity.java
@@ -0,0 +1,13 @@
+package com.example.practiceapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FragmentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
